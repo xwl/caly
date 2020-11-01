2bdc9c88a1bf592cc378295b102b69cc80e094c6
William Xu <william.xwl@gmail.com>
2020-11-01 16:34:38 +0100

    Change dos CRLF line ending to unix style

--- caly.el
+++ caly.el
@@ -1,5 +1,22 @@
 ;;; caly.el --- display year calendar
 
+;; Copyright (C) 2013 Ivan Kanis
+;; Author: Ivan Kanis
+;;
+;; This program is free software ; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation ; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY ; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 ;;; Commentary:
 
 ;; It displays a year calendar and holiday faces. When displaying the
@@ -173,25 +190,4 @@ line."
   ;; show all holidays of the year
   (list-holidays this-year)))
 
-
-;; Copyright (C) 2013 Ivan Kanis
-;; Author: Ivan Kanis
-;;
-;; This program is free software ; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation ; either version 2 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY ; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program ; if not, write to the Free Software
-;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-;;
-;; vi:et:sw=4:ts=4:
-;; Local Variables:
-;; compile-command: "make"
-;; End:
+;;; caly.el ends here
